3490b02f72666d330546172a17cc19d1fc8c25db
Luis Chacha <luischacha82@gmail.com>
2026-03-03 23:35:05 -0500

    sw mode network First

--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
-// sw.js - Service Worker básico
+const CACHE_NAME = "luis-dashboard-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "styles.css",
+  "test.html",
+];
+
+// Instalación inicial
 self.addEventListener("install", (event) => {
-  console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)),
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker activo");
-});
-
+// El corazón de la estrategia: NETWORK FIRST
 self.addEventListener("fetch", (event) => {
-  // Permite que la app funcione normalmente buscando en internet
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request)
+      .then((networkResponse) => {
+        // Si hay red, clonamos la respuesta y la guardamos en el caché
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => {
+        // Si falla la red (Modo Avión), servimos lo que tengamos en caché
+        return caches.match(event.request);
+      }),
+  );
 });
